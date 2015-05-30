dee4702990d6641b42db9575bf1e8d77049e2bfb
Kieran Uddin <contact@kieranitis.com>
2015-05-31 00:15:59 +0100

    v 0.2.4
    Allowed player to turn to face walls when next to wall.

--- src/com/bourneless/roguelike/entity/player/Player.java
+++ src/com/bourneless/roguelike/entity/player/Player.java
@@ -5,7 +5,6 @@ import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
 import com.bourneless.engine.main.Main;
-import com.bourneless.engine.math.Vector2;
 import com.bourneless.roguelike.entity.Entity;
 import com.bourneless.roguelike.entity.EntityType;
 import com.bourneless.roguelike.map.Map;
@@ -19,57 +18,62 @@ public class Player extends Entity {
 	}
 
 	public void paint(Graphics2D g) {
-		g.drawImage(image, pos.x + xOffset, pos.y - image.getHeight() / 2 + yOffset, null);
+		g.drawImage(image, pos.x + xOffset, pos.y - image.getHeight() / 2
+				+ yOffset, null);
 	}
 
 	public void update(int xOffset, int yOffset) {
 		this.xOffset = xOffset;
 		this.yOffset = yOffset;
 	}
-	
+
 	public void keyPressed(KeyEvent e, Map map) {
 		System.out.println(e.getKeyCode());
 		if (e.getKeyCode() == 65) {
 			// A Key
-			if (map.getRoom().getTiles()[(pos.x / 64) - 1][(pos.y / 64)].isPassable()) {
-				if (this.image.equals(Main.resourceLoader.player[1])) {
+			if (this.image.equals(Main.resourceLoader.player[1])) {
+				if (map.getRoom().getTiles()[(pos.x / 64) - 1][(pos.y / 64)]
+						.isPassable()) {
 					this.pos.x -= 64;
 					this.tile = map.getRoom().getTiles()[pos.x / 64][pos.y / 64];
-				} else {
-					this.image = Main.resourceLoader.player[1];
 				}
+			} else {
+				this.image = Main.resourceLoader.player[1];
 			}
 		} else if (e.getKeyCode() == 68) {
 			// D Key
-			if (map.getRoom().getTiles()[(pos.x / 64) + 1][(pos.y / 64)].isPassable()) {
-				if (this.image.equals(Main.resourceLoader.player[2])) {
+			if (this.image.equals(Main.resourceLoader.player[2])) {
+				if (map.getRoom().getTiles()[(pos.x / 64) + 1][(pos.y / 64)]
+						.isPassable()) {
 					this.pos.x += 64;
 					this.tile = map.getRoom().getTiles()[pos.x / 64][pos.y / 64];
-				} else {
-					this.image = Main.resourceLoader.player[2];
 				}
+			} else {
+				this.image = Main.resourceLoader.player[2];
 			}
 		}
 
 		if (e.getKeyCode() == 87) {
 			// W Key
-			if (map.getRoom().getTiles()[(pos.x / 64)][(pos.y / 64) - 1].isPassable()) {
-				if (this.image.equals(Main.resourceLoader.player[3])) {
+			if (this.image.equals(Main.resourceLoader.player[3])) {
+				if (map.getRoom().getTiles()[(pos.x / 64)][(pos.y / 64) - 1]
+						.isPassable()) {
 					this.pos.y -= 64;
 					this.tile = map.getRoom().getTiles()[pos.x / 64][pos.y / 64];
-				} else {
-					this.image = Main.resourceLoader.player[3];
 				}
+			} else {
+				this.image = Main.resourceLoader.player[3];
 			}
 		} else if (e.getKeyCode() == 83) {
 			// S Key
-			if (map.getRoom().getTiles()[(pos.x / 64)][(pos.y / 64) + 1].isPassable()) {
-				if (this.image.equals(Main.resourceLoader.player[0])) {
+			if (this.image.equals(Main.resourceLoader.player[0])) {
+				if (map.getRoom().getTiles()[(pos.x / 64)][(pos.y / 64) + 1]
+						.isPassable()) {
 					this.pos.y += 64;
 					this.tile = map.getRoom().getTiles()[pos.x / 64][pos.y / 64];
-				} else {
-					this.image = Main.resourceLoader.player[0];
 				}
+			} else {
+				this.image = Main.resourceLoader.player[0];
 			}
 		}
 	}
