efef97517c560daca0e719b46bcf713ba2d754b6
Kieran Uddin <contact@kieranitis.com>
2015-05-30 20:35:20 +0100

    v 0.1.4
    Added EntityType class with type integers

--- src/com/bourneless/roguelike/entity/Entity.java
+++ src/com/bourneless/roguelike/entity/Entity.java
@@ -5,11 +5,13 @@ import java.awt.image.BufferedImage;
 import com.bourneless.engine.math.Vector2;
 import com.bourneless.roguelike.map.tile.Tile;
 
+@SuppressWarnings("unused")
 public class Entity{
 	
 	private Vector2 pos;
 	private Tile tile;
 	private BufferedImage image;
+	protected int type;
 	
 	public Entity(Tile tile, BufferedImage image){
 		this.pos = new Vector2(tile.getPos().x, tile.getPos().y);
---
+++ src/com/bourneless/roguelike/entity/EntityType.java
@@ -0,0 +1,7 @@
+package com.bourneless.roguelike.entity;
+
+public class EntityType {
+	public static final int PLAYER = 0;
+	public static final int ENEMY = 1;
+	public static final int FIXED = 2;
+}
